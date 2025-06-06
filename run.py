eba421fe0e3aea22a8ed964259c0443fa84951be
renan-ald <renan.ald@hotmail.com>
2025-06-06 19:30:18 -0300

    ajuste editar para não aparecer o botão

--- run.py
+++ run.py
@@ -4,3 +4,9 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=False)
+#from app import create_app
+
+#app = create_app()
+
+#if _name_ == "_main_":
+    #app.run(ssl_context=("certs/cert.pem", "certs/key.pem"), host="0.0.0.0", port=5000, debug=True)
