c984ebe22d605455a87647e941e06add971e4982
renan-ald <renan.ald@hotmail.com>
2025-06-11 19:34:08 -0300

    paramentro data mes do dashboard agora salva alteração do user

--- run.py
+++ run.py
@@ -6,9 +6,10 @@ app = create_app()
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)
 
+#from waitress import serve
 #from app import create_app
 
 #app = create_app()
 
 #if _name_ == "_main_":
-    #app.run(ssl_context=("certs/cert.pem", "certs/key.pem"), host="0.0.0.0", port=5000, debug=True)
+    #serve(ssl_context=("certs/cert.pem", "certs/key.pem"), host="0.0.0.0", port=5000, debug=True)
